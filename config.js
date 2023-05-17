506828d9a348af1b2d26a1aff6e538fdb01108c5
ADITYA NARAYAN GUPTA <96142757+guptaadit82@users.noreply.github.com>
2023-05-17 17:32:29 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    JWT_SECRET : "secret",
-    EMAIL: "steve.franecki@ethereal.email", // testing email & password
-    PASSWORD : "sMf46xCzrvdrxvuagc",
-    ATLAS_URI: "mongodb+srv://happy8:happy8@bankdb.ovxwozg.mongodb.net/"
-}
